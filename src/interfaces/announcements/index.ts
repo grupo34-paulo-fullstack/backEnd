b542208520f87c7c6b752cf6551f231306dc5680
P4ul0Silva <paulors43@outlook.com>
2023-02-24 09:47:39 -0300

    added controllers for  announcement delete + edit  + entity fix price and createdAt fields

--- src/interfaces/announcements/index.ts
+++ src/interfaces/announcements/index.ts
@@ -1,1 +1,14 @@
-// example file
+import { Gallery } from "../../entities/gallery.entities";
+
+export interface IAnnouncement {
+    readonly id: string;
+    image: string;
+    model: string;
+    year: string;
+    km: string;
+    price: number;
+    readonly createdAt: Date;
+    is_active: boolean;
+    readonly user_id: string;
+    gallery?: Gallery[]
+}
